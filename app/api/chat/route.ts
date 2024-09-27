650f40bff5bbacf29b97797f21cb46710b300d0d
jc4f <lequan12122002@gmail.com>
2024-09-27 15:53:07 +0700

    feat: stream additional data

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,5 +1,5 @@
-import { google } from '@ai-sdk/google';
-import { streamText, convertToCoreMessages } from 'ai';
+import { google } from "@ai-sdk/google";
+import { streamText, convertToCoreMessages, StreamData } from "ai";
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
@@ -7,10 +7,16 @@ export const maxDuration = 30;
 export async function POST(req: Request) {
   const { messages } = await req.json();
 
+  const data = new StreamData();
+  data.append({ test: "value" });
+
   const result = await streamText({
-    model: google('gemini-1.5-pro-latest'),
+    model: google("gemini-1.5-pro-latest"),
     messages: convertToCoreMessages(messages),
+    onFinish() {
+      data.close();
+    },
   });
 
-  return result.toDataStreamResponse();
+  return result.toDataStreamResponse({ data });
 }
